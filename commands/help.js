56eb22101f0c277f6727e7f4dcad92a4af9fffa1
tracehybertson <trace.i.hybertson@gmail.com>
2021-09-20 22:18:34 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
     	.setAuthor('HelpBot', 'https://cdn.pixabay.com/photo/2017/05/13/09/04/question-2309040_1280.jpg', 'http://www.google.com')
     	.setDescription('So what can this bot do?')
       .addFields(
-        { name: 'We have a few commands you can use', value: "Be sure to type a '???' before each key word." },
+        { name: 'We have a few commands you can use', value: "Be sure to type a '%' before each key word." },
         { name: '\u200B', value: '\u200B' },
       )
       .addField('Some other commands to try:', 'Try using pong, and see what happens!', true)
